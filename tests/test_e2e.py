e3e7c4ec2d5da19b1e60fc62b25a8033014de15e
louiseLV <100593473+louiseLV@users.noreply.github.com>
2025-12-02 19:39:46 +0100

    Fix tests/lint issues

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -8,8 +8,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 BASE_URL = "http://127.0.0.1:5000"
 
