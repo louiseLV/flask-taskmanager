8a4cd402103426a2df84036f313a7b494e3f4eeb
louiseLV <100593473+louiseLV@users.noreply.github.com>
2025-12-02 16:57:03 +0100

    Fix tests/lint issues

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -4,14 +4,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 BASE_URL = "http://127.0.0.1:5001"
 
 
 @pytest.fixture(scope="module")
 def browser():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox") 
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     yield driver
     driver.quit()
 
