b21ee425f772d6b80b8738e6014937df03338248
louiseLV <100593473+louiseLV@users.noreply.github.com>
2025-12-03 09:46:29 +0100

    Fix tests/lint issues

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def register_routes(app):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=False, port=5000, host="0.0.0.0")
